a047dc1880e8e4e292d3969c80f43174d0cdab7d
Fabio Gomes Rocha <gomesrocha@gmail.com>
2022-04-04 19:43:20 -0300

    update files

--- domain/calculo_salario.py
+++ domain/calculo_salario.py
@@ -1,24 +1,26 @@
-class calculateSalary:
+class calculate_salary:
+    def __init__(self, gross_salary):
+        self.gross_salary = gross_salary
 
-    def calculate_inss(self, salario_bruto):
-        if salario_bruto < 1693.73:
-            inss = salario_bruto * 8 / 100
-        elif salario_bruto >= 1693.73 and salario_bruto < 2822.91:
-            inss = salario_bruto * 9 / 100
-        elif salario_bruto > 2822.90 and salario_bruto < 5645.80:
-            inss = salario_bruto * 11 / 100
+    def calculate_inss(self):
+        if self.gross_salary < 1693.73:
+            inss = self.gross_salary * 8 / 100
+        elif self.gross_salary >= 1693.73 and self.gross_salary < 2822.91:
+            inss = self.gross_salary * 9 / 100
+        elif self.gross_salary > 2822.90 and self.gross_salary < 5645.80:
+            inss = self.gross_salary * 11 / 100
         else:
             inss = 621.03
         return inss
 
-    def calculate_irrf(self, salario_bruto):
-        salario = salario_bruto - calculateSalary.calculate_inss(salario_bruto)
-        if salario >= 1903.99 and salario < 2826.66:
-            irrf = (salario - 142.82) * 7.5 / 100
-        elif salario >= 2826.66 and salario < 3751.06:
-            irrf = (salario - 354.80) * 15 / 100
-        elif salario >= 3751.05 and salario < 4664.69:
-            irrf = (salario - 636.13) * 22.5 / 100
-        elif salario >= 4664.69:
-            irrf = (salario - 869.36) * 27.5 / 100
+    def calculate_irrf(self):
+        salary = self.gross_salary - calculate_salary.calculate_inss()
+        if salary >= 1903.99 and salary < 2826.66:
+            irrf = (salary - 142.82) * 7.5 / 100
+        elif salary >= 2826.66 and salary < 3751.06:
+            irrf = (salary - 354.80) * 15 / 100
+        elif salary >= 3751.05 and salary < 4664.69:
+            irrf = (salary - 636.13) * 22.5 / 100
+        elif salary >= 4664.69:
+            irrf = (salary - 869.36) * 27.5 / 100
         return irrf
--- test/test_calculo_salario.py
+++ test/test_calculo_salario.py
@@ -1,9 +1,9 @@
 import unittest
-from domain.calculo_salario import calculateSalary
+from domain.calculo_salario import calculate_salary
 
 class MyTestCase(unittest.TestCase):
     def test_calculate_inss(self):
-        inss = calculateSalary.calculate_inss(1600)
+        inss = calculate_salary.calculate_inss(1600)
         self.assertEqual(inss, 128)  # add assertion here
 
 
